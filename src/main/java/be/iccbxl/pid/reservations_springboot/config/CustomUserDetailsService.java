95bd948c10c74bd4081a84742844126f1e5ff89d
Dugickaa <nermina111@hotmail.com>
2025-01-26 18:32:28 +0100

    Frontend authentification - logout (bug aussi sur le logout le message se connecter et vous n'etes pas connecté reste toujours visible quand on log out le message bob vous etes connecté est toujours visible aussi)
    I'll come back later on it

--- src/main/java/be/iccbxl/pid/reservations_springboot/config/CustomUserDetailsService.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/config/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package be.iccbxl.pid.reservations_springboot.config;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -21,17 +22,24 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByLogin(username);
+        final User user = userRepository.findByLogin(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("User " + username + " not found");
+        }
 
         return new org.springframework.security.core.userdetails.User(
-                user.getLogin(), user.getPassword(),
-                getGrantedAuthorities(user.getRole()));
+                username,
+                user.getPassword(),
+                getGrantedAuthorities(user.getRole().toString()));
     }
 
     private List<GrantedAuthority> getGrantedAuthorities(String role) {
         List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
         authorities.add(new SimpleGrantedAuthority("ROLE_" + role));
+
         return authorities;
     }
+
 }
 
