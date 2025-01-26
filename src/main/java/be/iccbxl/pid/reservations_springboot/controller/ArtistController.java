76c0c80d2f328dd0734ef3249b4fd6cbbba1fe81
Dugickaa <nermina111@hotmail.com>
2025-01-26 15:46:25 +0100

    Form create Artist

--- src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservations_springboot/controller/ArtistController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 import be.iccbxl.pid.reservations_springboot.model.Artist;
@@ -124,6 +125,26 @@ public class ArtistController {
         return "redirect:/artists/"+artist.getId();
     }
 
+    @GetMapping("/artists/create")
+    public String create(Model model) {
+        Artist artist = new Artist(null,null);
+
+        model.addAttribute("artist", artist);
+
+        return "artist/create";
+    }
+
+    @PostMapping("/artists/create")
+    public String store(@Valid @ModelAttribute("artist") Artist artist, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "artist/create";
+        }
+
+        service.addArtist(artist);
+
+        return "redirect:/artists/"+artist.getId();
+    }
 
 
 }
