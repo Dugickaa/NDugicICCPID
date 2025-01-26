11c90f551a9cc615998a3c4871b42d31c16b5f6a
Dugickaa <nermina111@hotmail.com>
2025-01-26 17:17:49 +0100

    Spring Security configuration

--- src/main/resources/db/migration/V2_1__create_users_table.sql
+++ src/main/resources/db/migration/V2_1__create_users_table.sql
@@ -10,4 +10,3 @@ CREATE TABLE `users` (
                          `created_at` datetime NOT NULL,
                          PRIMARY KEY(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
