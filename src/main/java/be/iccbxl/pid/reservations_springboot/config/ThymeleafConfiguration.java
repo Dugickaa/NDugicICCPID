eccb0d996bfe1e955ccee838ce52826230e48b8f
Dugickaa <99209821+Dugickaa@users.noreply.github.com>
2025-01-26 15:20:34 +0100

    Add files via upload

---
+++ src/main/java/be/iccbxl/pid/reservations_springboot/config/ThymeleafConfiguration.java
@@ -0,0 +1,24 @@
+package be.iccbxl.pid.reservations_springboot.config;
+import org.springframework.context.annotation.Bean;
+
+import org.springframework.context.annotation.Configuration;
+
+
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+
+
+
+@Configuration
+
+public class ThymeleafConfiguration {
+
+    @Bean
+
+    public LayoutDialect thymeleafDialect() {
+
+        return new LayoutDialect();
+
+    }
+
+}
